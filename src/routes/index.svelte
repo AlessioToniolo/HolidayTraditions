8255f453959ff1eca23583a32069a6ddbbcb3432
Alessio Toniolo <44009231+AlessioToniolo@users.noreply.github.com>
2022-06-07 00:01:25 +0000

    Index Edits

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,48 @@
-<h1 class="text-3xl font-bold underline text-secondary">
-    Hello world!
-  </h1>
+<div class="max-w-screen-lg mx-auto flex justify-center px-5 py-7">
+	<div class="flex-col">
+		<div class="flex items-center">
+			<div class="flex-initial flex-col">
+				<p class="text-7xl text-primary font-bold mb-2">Holiday Traditions</p>
+				<p class="text-xl font-semibold text-primary">
+					Holiday market with vendors from all over Georgia!
+				</p>
+				<div class="mt-5">
+					<button class="btn btn-primary">View Vendors</button>
+					<button class="btn btn-secondary ml-1">Maps</button>
+				</div>
+			</div>
+			<div class="max-w-md shadow-xl">
+				<img class="rounded-xl" src="/market-inside.jpg" alt="Market Inside Centennial Gym" />
+			</div>
+		</div>
+		<div class="pt-20">
+			<div class="max-w-fit flex justify-center bg-primary">
+				<div class="card w-96 bg-base-100 shadow-xl">
+					<figure><img src="/food-counter.PNG" alt="Shoes" height="225" /></figure>
+					<div class="card-body">
+						<h2 class="card-title">Date & Time</h2>
+						<p>
+							The Holiday Traditions Market will be held at Marist School on November x 2022. The
+							market will run from x to x.
+						</p>
+						<div class="card-actions justify-end">
+							<div class="badge badge-outline">11/0X/22</div>
+							<div class="badge badge-outline">Xam to Xpm</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="card w-96 bg-base-100 shadow-xl">
+					<figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" /></figure>
+					<div class="card-body">
+						<h2 class="card-title">Location</h2>
+						<p>If a dog chews shoes whose shoes does he choose?</p>
+						<div class="card-actions justify-end">
+							<button class="btn btn-primary">Buy Now</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
