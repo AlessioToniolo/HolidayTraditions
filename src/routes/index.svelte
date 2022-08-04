39313015554a8e194d2bc4e6d161d468146afda3
Alessio Toniolo <alessiotoniolo@alessios-air.lan>
2022-08-04 14:54:59 -0400

    Finished Index Page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,15 @@
 <div class="max-w-screen-lg mx-auto flex flex-col justify-center px-5 py-7">
-	<div class="flex items-center">
-		<div class="flex-initial flex-col text-center md:text-left">
-			<p class="text-7xl text-primary font-bold mb-2">Holiday Traditions</p>
-			<p class="text-xl font-semibold text-primary">
+	<div class="flex items-center justify-center lg:gap-x-9 lg:justify-between ">
+		<div class=" flex-initial flex-col text-center lg:text-left">
+			<p class="lg:mt-0 mt-5 lg:text-7xl text-6xl text-primary font-bold mb-2">
+				Holiday Traditions
+			</p>
+
+			<p class="lg:text-xl mt-2 lg:mt-0 text-lg font-semibold text-primary">
 				Holiday market with vendors from all over Georgia!
 			</p>
-			<div class="mt-5">
+
+			<div class="lg:mt-5 mt-4">
 				<button class="btn btn-primary">View Vendors</button>
 				<a
 					href="https://www.eventbrite.com/e/2022-marist-holiday-traditions-juried-vendor-application-registration-310600322927"
@@ -15,42 +19,72 @@
 						class="btn btn-secondary ml-1">Register</button
 					>
 				</a>
+				<a href="info/maps">
+					<button href="info/maps" class="btn btn-secondary ml-1">Maps</button>
+				</a>
 			</div>
 		</div>
-		<div class="max-w-md shadow-xl">
-			<div class="hidden md:contents">
+		<div class="lg:max-w-md md:max-w-sm max-w-xs shadow-xl">
+			<div class="hidden lg:contents">
 				<img class="rounded-xl" src="/market-inside.jpg" alt="Market Inside Centennial Gym" />
 			</div>
 		</div>
 	</div>
 
 	<div class="pt-20">
-		<div class="flex flex-wrap gap-x-5 justify-center">
+		<div class="flex flex-wrap gap-x-16 gap-y-10 justify-center">
 			<div class="card w-96 max-w-96 bg-base-100 shadow-xl">
 				<figure><img src="/correct-food.png" alt="Shoes" height="225" /></figure>
 				<div class="card-body">
 					<h2 class="card-title">Date & Time</h2>
 					<p>
-						The Holiday Traditions Market will be held at Marist School on November x 2022. The
-						market will run from x to x.
+						The Holiday Traditions Market will be held at Marist School on Saturday, <b
+							>November 5th</b
+						>, 2022 from <b>9</b> a.m. to <b>4</b> p.m.
 					</p>
 					<div class="card-actions justify-end">
-						<div class="badge badge-outline">11/0X/22</div>
-						<div class="badge badge-outline">Xam to Xpm</div>
+						<div class="badge badge-outline">11/05/22</div>
+						<div class="badge badge-outline">9am - 4pm</div>
 					</div>
 				</div>
 			</div>
-			<div class="w-24 h-24 bg-primary" />
+
 			<div class="card w-96 max-w-96 bg-base-100 shadow-xl">
 				<figure><img src="correct-entrance.jpeg" alt="Shoes" /></figure>
 				<div class="card-body">
 					<h2 class="card-title">Location</h2>
 					<p>
-						Complimentary shuttle service is avaliable from Perimeter Summit to Marist School. View
-						the maps to navigate Marist!
+						Complimentary shuttle service is avaliable from Perimeter Summit to <a
+							href="https://goo.gl/maps/UV2rfELf7VEo7sYn6"
+							class="underline hover:text-gray-200">Marist School</a
+						>. View the maps to navigate Marist!
 					</p>
 					<div class="card-actions justify-end">
 						<a href="info/maps"> <button class="btn btn-primary">Maps</button></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="pt-20 pb-10 flex justify-center">
+		<div class="max-w-xl">
+			<div class="card w-96 bg-base-100 shadow-xl image-full">
+				<figure><img src="partner-vendors-resized.PNG" alt="Shoes" /></figure>
+				<div class="card-body">
+					<h2 class="card-title">How to Register</h2>
+					<p>
+						Registration is $25.00 and completed on the <a
+							href="https://www.eventbrite.com/e/2022-marist-holiday-traditions-juried-vendor-application-registration-310600322927"
+							class="underline hover:text-gray-200">Eventbrite</a
+						>.
+					</p>
+					<div class="card-actions justify-end">
+						<a
+							href="https://www.eventbrite.com/e/2022-marist-holiday-traditions-juried-vendor-application-registration-310600322927"
+						>
+							<button class="btn btn-primary">Register</button></a
+						>
 					</div>
 				</div>
 			</div>
